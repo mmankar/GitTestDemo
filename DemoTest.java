f3baed3bd10f88c3d6c11b17a23ab3d189c782f2
mmankar <43125623+mmankar@users.noreply.github.com>
2023-03-12 22:12:43 +0530

    file1
    test to main

---
+++ DemoTest.java
@@ -0,0 +1,13 @@
+package com.gittest.demo;
+
+public class DemoTest {
+	public void methodTest1() {
+		
+		System.out.println("Test Git Repo");
+	}
+	public static void main(String[] args) {
+		DemoTest demoTest=new DemoTest();
+		demoTest.methodTest1();		
+	}
+
+}
